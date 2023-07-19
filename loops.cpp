ae7a7f3885e500d9d87670ec44d955d75027ba4d
shivamsharma809 <138153296+shivamsharma809@users.noreply.github.com>
2023-07-19 09:48:51 +0530

    Add files via upload

---
+++ loops.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+
+    for(i=1; i<101; i++)// called the update happens after the interation(part of a loop is completed)
+    {
+        cout<<i<<" ";
+    }
+
+}
